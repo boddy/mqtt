fc1259feec810c2050635afd05a22416ab73633c
body <bo_dy@163.com>
2023-04-07 17:29:48 +0800

    Create Events.php
    add plugin

---
+++ src/plugin/boddy/mqtt/Events.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace plugin\boddy\mqtt;
+
+class Events
+{
+    public static function onWorkerStart($worker)
+    {
+
+    }
+
+    public static function onConnect($client_id)
+    {
+
+    }
+
+    public static function onMessage($client_id, $message)
+    {
+
+    }
+
+    public static function onClose($client_id)
+    {
+
+    }
+
+    public static function OnError(\Exception $e)
+    {
+
+    }
+
+}
