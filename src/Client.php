4c690bc3f0f45e4d923ae7bff152034c624cfec5
body <bo_dy@163.com>
2023-04-21 11:08:17 +0800

    update
    update

--- src/Client.php
+++ src/Client.php
@@ -1,12 +1,20 @@
 <?php
 namespace Boddy\Mqtt;
 
+use Workerman\Worker;
+
 class Client
 {
     protected $eventHandler = null;
     protected $config = [];
     protected $option = null;
     protected $address = null;
+    
+    /**
+     * mqtt类型，区分不同的渠道，如车厂，iot
+     * @var null
+     */
+    protected $type = null;
 
 
     public function __construct($config)
@@ -17,17 +25,22 @@ class Client
         }
     }
 
-    public function onWorkerStart()
+    public function onWorkerStart(Worker $worker)
     {
         if (function_exists('opcache_reset')) {
             opcache_reset();
         }
         $mqtt = new \Workerman\Mqtt\Client($this->address, $this->option);
+        $this->eventHandler::$type = $this->type;
         foreach (['onConnect', 'onMessage', 'onClose', 'onError'] as $event) {
             if (method_exists($this->eventHandler, $event)) {
                 $mqtt->$event = [$this->eventHandler, $event];
             }
         }
+        
+        if (method_exists($this->eventHandler, 'onWorkerStart')) {
+            call_user_func([$this->eventHandler, 'onWorkerStart'], $worker);
+        } 
         $mqtt->connect();
     }
 
